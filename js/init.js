1b1c38855fbfbb98bddde4a62b8a4f4f14540a8d
naszczep <natalia.diana.szczepanek@cern.ch>
2026-06-29 14:28:36 +0200

    Updated style, fixed Firefox issues in UI and in the table, highlighted more useful things

--- js/init.js
+++ js/init.js
@@ -2,6 +2,8 @@
   $(function(){
 
     $('.button-collapse').sideNav();
+    $('.dropdown-button').dropdown();
+    $('.collapsible').collapsible({ accordion: true });
     $('.carousel').carousel();
 
   }); // end of document ready
